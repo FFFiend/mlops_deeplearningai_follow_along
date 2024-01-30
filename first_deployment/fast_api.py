29aad2fdebd3f2c802e941cfd0d92eb98f921adb
FFFiend <owais.zahid@mail.utoronto.ca>
2024-01-30 14:26:52 -0500

    ran rudimentary text generation pipeline locally.

--- first_deployment/fast_api.py
+++ first_deployment/fast_api.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from abc import ABC, abstractmethod
 from pydantic import BaseModel
 import transformers
+from transformers import AutoTokenizer, pipeline, AutoModelForCausalLM
 
 # host huggingface LLMs on a fastapi server 
 
@@ -13,10 +14,13 @@ app = FastAPI(title="skeleton deployment")
 def home():
     return "Welcome to the skeleton fastapi server"
     
-@app.get("/predict/{prompt}")
-def prediction(prompt: str):
-    #pipe = transformers.pipeline(model="facebook/bart-large-mnli")
-    output = prompt
+@app.get("/predict/{model_id}/{prompt}")
+def prediction(prompt: str, model_id: str):
+    model_id = "gpt2"
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(model_id)
+    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10)
+    output = pipe(prompt)
     return output
 
 uvicorn.run(app, host="127.0.0.1", port=3000, root_path="/serve")
